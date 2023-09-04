3d51e1158371248604ad278733f87b627286a48c
emmanuelonwukwe <emmanuelonwukwe202@gmail.com>
2023-09-04 16:15:49 +0100

    Fix the bot not responding bug

--- bot.js
+++ bot.js
@@ -23,13 +23,19 @@ for (const folder of commandFolders) {
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
+
 		if ('data' in command && 'execute' in command) {
-			commands.push(command.data.toJSON());
+            //This helps to set this command. Else the bot will not be able to respond
+            client.commands.set(command.data.name, command);
+
+			commands.push(command.data);
+
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
 	}
 }
+
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
