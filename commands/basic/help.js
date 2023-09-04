ce8d25c394398e643660f906508e95360f458fc4
Chizaram-Igolo <chizaram.igolo@yahoo.com>
2023-09-04 15:40:52 +0100

    🧰⚒️ refactor: Refactor 'Require' Statements to ES6 Imports (#2)

--- commands/basic/help.js
+++ commands/basic/help.js
@@ -1,10 +1,12 @@
-const { SlashCommandBuilder } = require('discord.js');
+import { SlashCommandBuilder } from "discord.js";
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('help')
-		.setDescription('Get information on how voting!'),
-	async execute(interaction) {
-		await interaction.reply('Here are the basic information to help you get started with the app!');
-	},
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("Get information on how voting!"),
+  async execute(interaction) {
+    await interaction.reply(
+      "Here are the basic information to help you get started with the app!"
+    );
+  },
 };
